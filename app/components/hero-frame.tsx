1217a40c758f75d1629eb132505bad252ef02dd3
amarmanhala <amar.manhala@gmail.com>
2025-10-22 02:26:50 -0400

    Add hero section

---
+++ app/components/hero-frame.tsx
@@ -0,0 +1,19 @@
+const HeroFrame = () => {
+  return (
+    <div className="flex flex-col w-full items-center justify-center gap-16">
+      
+      <div className="border-12 w-[300px] h-[450px] border-black rounded-sm">
+        <h1 className="text-7xl">Beyond</h1>
+        <h1 className="text-9xl">Frames</h1>
+      </div>
+
+      <div className="">
+        <button className="bg-black text-white p-2 rounded-sm cursor-pointer">
+          Shop now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HeroFrame;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import HeroFrame from "./components/hero-frame";
 
 export default function Home() {
   return (
     <div>
       <main>
-       Hi
+       <HeroFrame />
       </main>
     </div>
   );
