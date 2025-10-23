6b4c98ea1eab6084fc647efc3d6b2dcd7fc0af90
amarmanhala <amar.manhala@gmail.com>
2025-10-22 22:19:45 -0400

    Add images

--- app/components/hero-frame.tsx
+++ app/components/hero-frame.tsx
@@ -1,15 +1,45 @@
+import Image from "next/image";
+
 const HeroFrame = () => {
   return (
-    <div className="flex flex-col w-full items-center justify-center gap-16">
-      
-      <div className="border-12 w-[300px] h-[450px] border-black rounded-sm">
-        <h1 className="text-7xl">Beyond</h1>
-        <h1 className="text-9xl">Frames</h1>
+    <div className=" grid grid-cols-4 gap-8 p-16">
+      <div className="">
+        <Image
+          src="/art1.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </div>
+      <div className="">
+        <Image
+          src="/art3.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </div>
+      <div className="">
+        <Image
+          src="/art4.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
       </div>
 
       <div className="">
-        <button className="bg-black text-white p-2 rounded-sm cursor-pointer">
-          Shop now
+        <Image
+          src="/art7.png"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </div>
+
+      <div className="text-center col-span-4">
+        <button className="bg-black px-4 text-white py-2 cursor-pointer">
+          Shop Now
         </button>
       </div>
     </div>
---
+++ app/products/arts.ts
@@ -0,0 +1,13 @@
+export const products = [{
+    id: "1",
+    description: "This is art of girl",
+    price: "$25",
+    size: {
+        1: "8 X 12",
+        2: "12 X 16",
+    },
+    discount: "",
+    imageURL: "",
+    videoURL: "",
+    createtdAt: "",
+}];
